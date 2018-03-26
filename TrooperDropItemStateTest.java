1d898dc568bfd351c1b90c04267ade502e05b48c
sdt1001 <sdt1001@wildcats.unh.edu>
2018-03-25 21:37:14 -0400

    Create TrooperDropItemStateTest.java

---
+++ TrooperDropItemStateTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TrooperDropItemStateTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		tsm =! tsmStateMachine
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
